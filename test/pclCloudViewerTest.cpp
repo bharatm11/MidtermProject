f31caa80c67e92f2d2b3f4ff4ae431bb9c00a2d1
MichaelKam <mkam@terpmail.umd.edu>
2017-10-13 12:10:52 -0700

    left main.cpp in cpp-test

--- test/pclCloudViewerTest.cpp
+++ test/pclCloudViewerTest.cpp
@@ -5,25 +5,3 @@
  *      Author: Michael
  */
 #include <gtest/gtest.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <iostream>
-#include "pclIo.h"
-
-#include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/surface/mls.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/filters/passthrough.h>
-//#include "pclCloudViewer.h"
-/*
-TEST(pclCloudViewerTest, MustReturnUserdataEqualto1) {
-  pclCloudViewer pclView;
-  pclIo pclIo;
-  pclIo.readPCDfile("pclIo_test.pcd");
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-  pclIo.getPointCloud(*cloud);
-  pclView.dispaly(cloud);
-  EXPECT_NEAR(1, pclView.user_data, 0.5);
-}
- */
