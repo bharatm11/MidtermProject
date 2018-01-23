bb65c9e76a38c719472bd035b822d225d7f4c08e
MichaelKam <mkam@terpmail.umd.edu>
2018-01-23 12:10:52 -0800

    ____

---
+++ include/mergeHanhsPoint.h
@@ -0,0 +1,20 @@
+// "Copyright [2017] <Michael Kam>"
+/** @file mergeHanhsPoint.h
+ *  @brief header file of an mergeHanhsPoint class.
+ *
+ *  This class reads a .txt file(which was used to provide by Hanh to specify the point marked on
+ *  the surface of kidney), putting
+ *
+ *  @author Michael Kam (michael081906)
+ *  @bug No known bugs.
+ *  @copyright GNU Public License.
+ */
+
+#ifndef INCLUDE_MERGEHANHSPOINT_H_
+#define INCLUDE_MERGEHANHSPOINT_H_
+
+
+
+
+
+#endif  // INCLUDE_MERGEHANHSPOINT_H_
